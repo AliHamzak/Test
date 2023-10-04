966ae24f164c77e5d05c422b3fcc38fb89583b25
Hamza <alihamzabitsclan945@gmail.com>
2023-10-04 16:06:16 +0500

    developer commit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 $btn = document.getElementById('btn');
 $text = document.getElementById('text');
 $btn.addEventListener('click', function () {
-    length(text.textContent);
-    console.log(":hello");
+    alert(text.textContent);
 })
